06c755543342af1d460a956f6aa5f98d8aa1e4f4
Fill <wreaths.outset-92@icloud.com>
2025-04-27 19:38:25 +0200

    Create random_password.py

---
+++ random_password.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+length = int(input("🔢 Password length: "))
+chars = string.ascii_letters + string.digits + "!@#$%^&*()"
+password = ''.join(random.choice(chars) for _ in range(length))
+
+print(f"Generated Password: {password}")
